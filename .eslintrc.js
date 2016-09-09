e634a44ee4df5bf5b885dd2e69996cc4a6cc70a2
xLsDg <xlsdg@qq.com>
2016-09-09 11:24:07 +0800

    1.更新新模板；

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,8 +12,11 @@ module.exports = {
   ],
   // add your custom rules here
   'rules': {
+    // allow paren-less arrow functions
     'arrow-parens': 0,
+    // allow async-await
     'generator-star-spacing': 0,
+    // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'no-extra-semi': 1,
     'no-inner-declarations': [2,'both'],
